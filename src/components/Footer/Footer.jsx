77067c302180b7e40356ad4b3ad20c33087abb42
Graciela <graci.romano@gmail.com>
2026-03-03 17:05:29 -0300

    Se agrego el favicon y se agrego el icon en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,59 +7,65 @@ const Footer = () => {
     <footer className="footer3 cid-footer">
       <div className="container">
         <div className="row">
-          <div className="row-links">
-            <ul className="header-menu">
-              <li className="header-menu-item">
-                <Link to="/" className="text-white">Inicio</Link>
-              </li>
-              <li className="header-menu-item">
-                <Link to="/eventos" className="text-white">Eventos</Link>
-              </li>
-              <li className="header-menu-item">
-                <Link to="/nosotros" className="text-white">Nosotros</Link>
-              </li>
-              <li className="header-menu-item">
-                <Link to="/contacto" className="text-white">Contacto</Link>
-              </li>
-            </ul>
+          <div className="col-12 col-md-6 col-lg-4 ">
+            <img src="/src/assets/images/icon/aroma-a-cafe-logo2.1.png" alt="CafeCorp Logo" className="footer-logo" />
           </div>
-
-          <div className="social-section">
-            <h3 className="social-title">
-              <strong>Síguenos</strong>
-            </h3>
-            <div className="social-row">
-              <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
-                <i className="fab fa-instagram"></i>
-              </a>
-              <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
-                <i className="fab fa-linkedin-in"></i>
-              </a>
+          <div className="col-12 col-md-6 col-lg-4 ">
+            <div className="row-links">
+              <ul className="header-menu">
+                <li className="header-menu-item">
+                  <Link to="/" className="text-white">Inicio</Link>
+                </li>
+                <li className="header-menu-item">
+                  <Link to="/eventos" className="text-white">Eventos</Link>
+                </li>
+                <li className="header-menu-item">
+                  <Link to="/nosotros" className="text-white">Nosotros</Link>
+                </li>
+                <li className="header-menu-item">
+                  <Link to="/contacto" className="text-white">Contacto</Link>
+                </li>
+              </ul>
             </div>
-          </div>
 
-          <div className="col-12 mt-4">
-            <p className="copyright">
-              © {new Date().getFullYear()} Aroma a Café. Todos los derechos reservados.
-            </p>
-          </div>
+            <div className="social-section">
+              <h3 className="social-title">
+                <strong>Síguenos</strong>
+              </h3>
+              <div className="social-row">
+                <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="" target="_blank" rel="noopener noreferrer" className="social-item">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
 
-          {/* Botón flotante de WhatsApp */}
-          <a
-            href="https://wa.me/5491158659887"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="whatsapp-float"
-            aria-label="Contactar por WhatsApp"
-          >
-            <i className="fab fa-whatsapp" />
-          </a>
+            <div className="col-12 mt-4">
+              <p className="copyright">
+                © {new Date().getFullYear()} Aroma a Café. Todos los derechos reservados.
+              </p>
+            </div>
+
+            {/* Botón flotante de WhatsApp */}
+            <a
+              href="https://wa.me/5491158659887"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="whatsapp-float"
+              aria-label="Contactar por WhatsApp"
+            >
+              <i className="fab fa-whatsapp" />
+            </a>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 "></div>
         </div>
       </div>
     </footer>
