f3894691a3e1e9ae1b1399ab1c380f3f559a0664
Graciela <graci.romano@gmail.com>
2026-05-02 10:38:08 -0300

    Fix: Corrijo el error del navbar que quedaba abierto en modo mobile.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
+
+  const closeMenu = () => setIsMenuOpen(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,13 +18,17 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location.pathname]);
+
   return (
     <section data-bs-version="5.1" className="menu menu2 cid-uXQ7jkaKzP" once="menu" id="menu-5-uXQ7jkaKzP">
       <nav className={`navbar navbar-dropdown navbar-fixed-top navbar-expand-lg ${isScrolled ? 'scrolled' : ''}`}>
         <div className="container-navbar">
           <div className="navbar-brand">
             <span className="navbar-logo">
-              <Link to="/">
+              <Link to="/" onClick={closeMenu}>
                 <img src="/src/assets/images/icon/aroma-a-cafe-logo2.1.png" alt="CafeCorp Logo"  />
               </Link>
             </span>
@@ -47,21 +54,21 @@ const Navbar = () => {
           <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`}>
             <ul className="navbar-nav nav-dropdown">
               <li className="nav-item">
-                <Link to="/" className="nav-link link text-black display-4">Inicio</Link>
+                <Link to="/" className="nav-link link text-black display-4" onClick={closeMenu}>Inicio</Link>
               </li>
               <li className="nav-item">
-                <Link to="/eventos" className="nav-link link text-black display-4">Eventos</Link>
+                <Link to="/eventos" className="nav-link link text-black display-4" onClick={closeMenu}>Eventos</Link>
               </li>
               <li className="nav-item">
-                <Link to="/nosotros" className="nav-link link text-black display-4">Nosotros</Link>
+                <Link to="/nosotros" className="nav-link link text-black display-4" onClick={closeMenu}>Nosotros</Link>
               </li>
               <li className="nav-item">
-                <Link to="/contacto" className="nav-link link text-black display-4">Contacto</Link>
+                <Link to="/contacto" className="nav-link link text-black display-4" onClick={closeMenu}>Contacto</Link>
               </li>
             </ul>
             
             <div className="navbar-buttons mbr-section-btn">
-              <Link to="/contacto" className="btn btn-primary display-4">Presupuesto</Link>
+              <Link to="/contacto" className="btn btn-primary display-4" onClick={closeMenu}>Presupuesto</Link>
             </div>
           </div>
         </div>
